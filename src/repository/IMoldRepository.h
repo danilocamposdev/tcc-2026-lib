f07ca5a21e69e19f16a0461274699593c6d60ac5
danilocamposdev <danilocampos.dev@gmail.com>
2026-06-26 11:45:08 -0300

    fix: ajust in mold repository

--- src/repository/IMoldRepository.h
+++ src/repository/IMoldRepository.h
@@ -1,14 +1,12 @@
 #pragma once
-#include <vector>
 #include "../model/Mold.h"
+#include <vector>
+#include <optional>
 
 class IMoldRepository {
-	public:
-		virtual void add(Mold mold) = 0;
-
-		virtual const Mold* get_by_id(int id) const = 0;
-
-		virtual const std::vector<Mold>& all() const = 0;
-
-		virtual ~IMoldRepository() = default;
+public:
+    virtual void add(Mold mold) = 0;
+    virtual std::optional<Mold> get_by_id(int id) const = 0;
+    virtual std::vector<Mold> all() const = 0;
+    virtual ~IMoldRepository() = default;
 };
