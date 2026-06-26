e413c38c7f39180086471d8c6d9300cf42cf8711
danilocamposdev <danilocampos.dev@gmail.com>
2026-06-26 15:00:41 -0300

    fix: save returns persisted Order

--- src/model/Order.h
+++ src/model/Order.h
@@ -1,20 +1,23 @@
 #pragma once
 #include "Date.h"
+#include <optional>
 
 class Order {
 	private:
-		int id_;
+		std::optional<int> id_;
 		Date delivery_date_;
 		int mold_id_;
 		int client_id_;
 		int quantity_;
 		int priority_;
 	public:
-		Order(int id, Date delivery_date, int mold_id, int client_id, int quantity, int priority)
-			: id_{id}, delivery_date_{delivery_date}, mold_id_{mold_id},
+		Order(Date delivery_date, int mold_id, int client_id, int quantity, int priority)
+			: delivery_date_{delivery_date}, mold_id_{mold_id},
 			client_id_{client_id}, quantity_{quantity}, priority_{priority} {}
 
-		int id() const { return id_; }
+		std::optional<int> id() const { return id_; }
+		void setId(int id) { id_ = id; }
+		bool isPersisted() const { return id_.has_value(); }
 		Date delivery_date() const { return delivery_date_; }
 		int mold_id() const { return mold_id_; }
 		int client_id() const { return client_id_; }
