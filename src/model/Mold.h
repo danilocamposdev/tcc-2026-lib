0005c139a75e904eeb83eb4c516327382af8a8c6
danilocamposdev <danilocampos.dev@gmail.com>
2026-06-26 13:04:23 -0300

    fix: mold id set optional

--- src/model/Mold.h
+++ src/model/Mold.h
@@ -1,17 +1,20 @@
 #pragma once
 #include <string>
+#include <optional>
 
 class Mold {
 	private:
-	int id_;
-    std::string type_;
-    int available_quantity_;
+		std::optional<int> id_;
+		std::string type_;
+		int available_quantity_;
 
 	public:
-	Mold(int id, std::string type, int available_quantity):
-		id_{id}, type_ {type}, available_quantity_ {available_quantity} {};
+		Mold(std::string type, int available_quantity):
+			type_ {type}, available_quantity_ {available_quantity} {};
 
-	int id() const {return id_;}
-	std::string type() const {return type_;}
-	int available_quantity() const {return available_quantity_;}
+		std::optional<int> id() const { return id_; }
+		void setId(int id) { id_ = id; }
+		bool isPersisted() const { return id_.has_value(); }
+		std::string type() const {return type_;}
+		int available_quantity() const {return available_quantity_;}
 };
--- src/repository/IMoldRepository.h
+++ src/repository/IMoldRepository.h
@@ -5,7 +5,7 @@
 
 class IMoldRepository {
 public:
-    virtual void add(Mold mold) = 0;
+    virtual void save(Mold mold) = 0;
     virtual std::optional<Mold> get_by_id(int id) const = 0;
     virtual std::vector<Mold> all() const = 0;
     virtual ~IMoldRepository() = default;
