32bddfb961a259f4b9b089e5ab20f10c61ac994b
danilocamposdev <danilocampos.dev@gmail.com>
2026-06-28 19:44:36 -0300

    feat: add setName

--- src/model/Client.h
+++ src/model/Client.h
@@ -14,4 +14,5 @@ class Client {
 		void setId(int id) { id_ = id; }
 		bool isPersisted() const { return id_.has_value(); }
 		std::string name() const { return name_; }
+		void setName(std::string name) { name_ = name;}
 };
